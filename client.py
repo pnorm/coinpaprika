a045ddd38f0c4da68ffabd47ebc6e779fe6a2c04
Paweł Normann <pawel.normann@gmail.com>
2021-04-06 14:56:12 +0200

    Handling HTTP reponse status codes.

--- client.py
+++ client.py
@@ -4,7 +4,9 @@ import json
 from typing import Dict, List, Union
 
 # 3rd party libraries
+import re
 import requests
+import sys
 
 
 class Client:
@@ -20,6 +22,23 @@ class Client:
             "end": end,
         }
 
+    def _check_status_codes(self, response) -> bool:
+        """Check response code. HTTP response status codes indicate whether a
+        specific HTTP request has been successfully completed.
+            Successful responses (200–299)
+            Client errors (400–499)
+            Server errors (500–599)
+        """
+        if re.match("^5[0-9]{2}$", str(response.status_code)):
+            print("Can't fetch the data. The issue is on coinparprika's side.")
+            sys.exit()
+        elif re.match("^4[0-9]{2}$", str(response.status_code)):
+            if reponse.status_code == 429:
+                print("Breaking a request rate limit. Single IP address can \
+send less than 10 requests per second")
+            print("Invalid requests ")
+            sys.exit()
+
     def get_historical_values(self) -> Union[List[Dict], None]:
         """
         Open/High/Low/Close values with volume and market_cap.
@@ -34,6 +53,7 @@ class Client:
             print("Connection problem. Check arguments.")
         else:
             # print(f"Connection succeed for url:\n{response.url}")
+            self._check_status_codes(response)
             return response.json()
 
     def get_coin_symbols(self) -> List[Dict[str, Union[str, int]]]:
